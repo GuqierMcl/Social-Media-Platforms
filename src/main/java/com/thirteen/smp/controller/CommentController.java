737a4146ff1787689c9cdd728cb9f585b031edf4
Guqier <GuqierSSR@vip.qq.com>
2023-04-18 09:02:41 +0800

    顾建平实现用户登录、注册、登出接口，添加Post、Comment模块的相关类，添加TODO信息

--- src/main/java/com/thirteen/smp/controller/CommentController.java
+++ src/main/java/com/thirteen/smp/controller/CommentController.java
@@ -3,6 +3,12 @@ package com.thirteen.smp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 评论模块控制器
+ * @author 庄可欣
+ * @version 1.0
+ * @since 1.0
+ */
 @RestController
 @RequestMapping("/comment")
 public class CommentController {
